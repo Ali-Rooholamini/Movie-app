e06ef2e2edd1f49c35452e056519837794d2c85f
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2022-08-05 16:56:14 +0430

    movie-app: first commit

---
+++ components/template/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="main-header">
+    <div class="main-header_content-wrapper">
+      <h4>Now Streaming</h4>
+      <h2>Now Streaming</h2>
+      <div class="main-header_button">
+        <button>View Movies</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+.main-header {
+  width: 100%;
+  height: 400px;
+  background-color: red;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <MainHeader />
+    <Nuxt />
+  </section>
+</template>
+
+<script>
+import MainHeader from '~/components/template/MainHeader.vue'
+export default {
+  name: 'MainPage',
+  components: {
+    MainHeader,
+  },
+}
+</script>
